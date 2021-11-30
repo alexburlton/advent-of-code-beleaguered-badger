e457430052b4632a67c6f75c55419bd7daaece57
Alex <alex.burlton@sonocent.com>
2021-11-30 20:22:23 +0000

    working (albeit slow) implementation for 2020 day 1

---
+++ bin/day_1_2020/day_1_2020.dart
@@ -0,0 +1,17 @@
+import 'package:trotter/trotter.dart';
+
+import '../utils/utils.dart';
+
+void main(List<String> arguments) {
+  final inputLines = readIntegerList('day_1_2020/input.txt');
+
+  print('Part A: ${getExpenseReportProduct(inputLines, 2)}');
+  print('Part B: ${getExpenseReportProduct(inputLines, 3)}');
+}
+
+num getExpenseReportProduct(List<int> report, int count) {
+  final correctCombos = report.combinations(count)().where((combo) => sumsTo2020(combo));
+  return correctCombos.first.product();
+}
+
+bool sumsTo2020(List<num> list) => list.sum() == 2020;
---
+++ bin/utils/utils.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+extension ListUtils on List<num> {
+  num sum() => reduce((value, element) => value + element);
+  num product() => fold(1, (value, element) => value * element);
+}
+
+List<int> readIntegerList(String filename) =>
+ readStringList(filename).map((line) => int.parse(line)).toList();
+
+List<String> readStringList(String filename) {
+  final input = File('bin/$filename').readAsStringSync();
+  return input.split('\n').toList();
+}

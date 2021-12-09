722b4cfa2bd3eaf84e6fc1205fd207c6ed6d663e
Alex <alex.burlton@sonocent.com>
2021-12-09 10:10:19 +0000

    part B

--- lib/day_9/day_9.dart
+++ lib/day_9/day_9.dart
@@ -6,6 +6,7 @@ import 'package:kt_dart/kt.dart';
 void main(List<String> arguments) {
   final input = readIntegerGrid('day_9/input.txt');
   partA(input);
+  partB(input);
 }
 
 
@@ -24,8 +25,30 @@ bool _isLowerThanNeighbours(Point pt, KtMap<Point, int> heightMap) {
 }
 
 void partB(KtMap<Point, int> heightMap) {
-  final points = heightMap.keys;
-  final lowPoints = points.filter((pt) => _isLowerThanNeighbours(pt, heightMap));
-  final lowRiskLevels = lowPoints.map((lowPt) => heightMap.getValue(lowPt) + 1);
-  print(lowRiskLevels.sum());
+  var allBasinPoints = _filterToBasinPoints(heightMap.keys, heightMap);
+  final basins = mutableListOf<KtSet<Point>>();
+  while (allBasinPoints.isNotEmpty()) {
+    final pt = allBasinPoints[0];
+    final basin = _exploreBasin(pt, heightMap);
+    basins.add(basin);
+    allBasinPoints = allBasinPoints - basin;
+  }
+
+  final basinSizes = basins.map((basin) => basin.size).sortedDescending();
+  print(basinSizes[0] * basinSizes[1] * basinSizes[2]);
+}
+
+KtList<Point> _filterToBasinPoints(KtSet<Point> points, KtMap<Point, int> heightMap)
+  => points.filter((pt) => heightMap[pt] != 9 && heightMap[pt] != null);
+
+KtSet<Point> _exploreBasin(Point startingPt, KtMap<Point, int> heightMap) {
+  KtSet<Point> prevNeighbours = emptySet();
+  var currentNeighbours = setOf(startingPt);
+  while ((currentNeighbours - prevNeighbours).isNotEmpty()) {
+    prevNeighbours = currentNeighbours;
+    final newNeighbours = prevNeighbours.flatMap((pt) => getNeighbourPoints(pt)).toSet() + prevNeighbours;
+    currentNeighbours = _filterToBasinPoints(newNeighbours, heightMap).toSet();
+  }
+
+  return currentNeighbours;
 }
